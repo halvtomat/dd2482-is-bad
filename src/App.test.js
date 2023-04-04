62532fadd260a6ada1f99e0c5d55aa729ea6e31c
Daniel Gustafsson <gustafsson.daniel.erik@outlook.com>
2023-04-04 11:50:14 +0000

    add failing test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('failing test', () => {
+    render(<App />);
+    const element = screen.getByText(/gibberish that dont exist/i);
+    expect(element).toBeInTheDocument();
+});
+
